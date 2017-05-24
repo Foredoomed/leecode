160ab0bfc133c4400ea454b1122617f307dafefd
Xuan Liu <foredoomed@Xuans-MacBook-Pro.local>
2017-05-24 17:23:31 -0400

    commit

---
+++ HammingDistance_461.java
@@ -0,0 +1,13 @@
+
+public class HammingDistance_461 {
+
+	public static void main(String[] args){
+		HammingDistance_461 hd = new HammingDistance_461();
+		int r = hd.hammingDistance(1, 4);
+		System.out.println(r);
+	}
+
+	int hammingDistance(int x, int y) {
+		return Integer.bitCount(x ^ y);
+	}
+}
