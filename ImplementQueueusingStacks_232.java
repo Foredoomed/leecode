37372f28c76eadcac617d3e6aba361aa99129c5e
foredoomed <foredoomed.org>
2017-08-27 15:50:09 +0800

    Add #232

---
+++ ImplementQueueusingStacks_232.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class ImplementQueueusingStacks_232 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public void pop() {
+        peek();
+        output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+
+}
