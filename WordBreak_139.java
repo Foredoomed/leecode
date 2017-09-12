d8f9f0e42395d1e1c0fe562444b794e7280fc309
foredoomed <mc.liuxuan@gmail.com>
2017-09-12 23:32:09 +0800

    Add #139

---
+++ WordBreak_139.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class WordBreak_139 {
+
+	public boolean wordBreak(String s, List<String> wordDict) {
+		boolean[] f = new boolean[s.length() + 1];
+
+		f[0] = true;
+
+		
+		for (int i = 1; i <= s.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (f[j] && wordDict.contains(s.substring(j, i))) {
+					f[i] = true;
+					break;
+				}
+			}
+		}
+
+		return f[s.length()];
+	}
+
+}
